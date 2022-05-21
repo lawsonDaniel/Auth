92eda60f83d11d12732ee7e579296757cb8445b1
lawsondaniel <lawblaze4@gmail.com>
2022-05-21 10:45:00 +0100

    added the side bar to the profile

--- src/components/Private/Profile.js
+++ src/components/Private/Profile.js
@@ -3,12 +3,30 @@ import React from 'react'
 
 const Profile = () => {
    
+  const container ={
+    display:"flex",
+    flexWrap:'wrap'
+  }
   
-   
+  const sidebar = {
+    background:'#111',
+    padding:'10px',
+    width:'20vw',
+    flex:'1',
+    height:'100vh'
+  }
+  const mainbar = {
+    background:'#999',
+   flex:'4',
+    height:'100vh'
+  }
+
+
   return (
       
-    <div>
-   profile
+    <div style={container}>
+         <div style={sidebar}>Side bar</div>
+         <div style={mainbar}>Main Area</div>
     </div>
   )
 }
