1ac992d4fa3dcca87ee60e84d4ec9e944b197bc1
lawsondaniel <lawblaze4@gmail.com>
2022-06-17 10:31:40 +0100

    some changes

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,33 @@ import { Link,Route } from 'react-router-dom'
 import Header from './components/Header'
 
 const Home = () => {
+  const progres = [10,20,30,40,50,60,70,80,90,100]
+  const active = []
+  const cal = (value)=>{
+    // console.log(value)
+  let max = progres[progres.length-1]
+  let rem =max - value
+  // console.log(rem)
+
+  for(let i=0;i<=progres.length;i++){
+    // console.log(progres[i])
+    let comp = progres[i]
+    if(rem === comp){
+    let de = comp+10
+    let index = progres.indexOf(de)
+    for(let i =0;i<index;i++){
+     let rem = progres[i]
+     active.push(rem)
+    if(active.length >= index)
+    active.length = 0
+     console.log(active)
+    }
+    }
+
+  }
+
+  }
+ 
   return (
     <div>
        <Header/>
@@ -12,6 +39,15 @@ const Home = () => {
   <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
     <main className="px-3">
       <h1>Login System</h1>
+      {
+        progres.map((btn)=>{
+          return(
+            <button key={btn} onClick={()=>{
+            return  cal(btn)
+            }}>{btn}</button>
+          )
+        })
+      }
       <p className="lead">This is a user authentication system built with react and firebase</p>
     </main>
     {/* <footer className="mt-auto text-white-50">
